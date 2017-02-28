488209faedd0480b43d03bbba7c6d880b33e402e
Ekta Nijhawan <ektanijhawan1@gmail.com>
2017-03-01 00:06:27 +0530

    Widget Problem - Sustains

--- app/src/main/java/com/example/ekta/noir/widget/WidgetService.java
+++ app/src/main/java/com/example/ekta/noir/widget/WidgetService.java
@@ -1,6 +1,7 @@
 package com.example.ekta.noir.widget;
 
 import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -10,6 +11,9 @@ import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.target.AppWidgetTarget;
 import com.example.ekta.noir.R;
 import com.example.ekta.noir.activities.BaseActivity;
 import com.example.ekta.noir.model.UnsplashData;
@@ -27,7 +31,8 @@ public class WidgetService extends RemoteViewsService {
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
         return new StackRemoteViewsFactory(this.getApplicationContext(), intent);
     }
-    UnsplashData photo= new UnsplashData();
+
+    UnsplashData photo = new UnsplashData();
 
     /**
      * Equivalent to a CursorAdapter/ArrayAdapter with ListView.
@@ -49,21 +54,23 @@ public class WidgetService extends RemoteViewsService {
 
         @Override
         public void onCreate() {
-unsplashList= new ArrayList<>();
-           getPhotos();
+            unsplashList = new ArrayList<>();
+            getPhotos();
 
         }
-        private void getPhotos(){
+
+        private void getPhotos() {
 
             ArrayList<UnsplashData> list = new ArrayList<>(UnsplashProviderMethods.getPhotoList(getApplicationContext()));
             //
             // unsplashList.clear();
-            for( UnsplashData photo : list){
+            for (UnsplashData photo : list) {
                 unsplashList.add(photo);
             }
-            Log.d("count",String.valueOf(unsplashList.size()));
+            Log.d("count", String.valueOf(unsplashList.size()));
             Collections.reverse(unsplashList);
         }
+
         /**
          * Called when notifyDataSetChanged() is called.
          * Hence we can update the widget with new data!
@@ -80,35 +87,57 @@ unsplashList= new ArrayList<>();
         @Override
         public int getCount() {
             //Meta-function for the AppWidgetManager
-            Log.d("widget count",String.valueOf(unsplashList.size()));
+            Log.d("widget count", String.valueOf(unsplashList.size()));
 
             return unsplashList.size();
         }
+        public AppWidgetTarget appWidgetTarget;
+
 
         @Override
         public RemoteViews getViewAt(int position) {
+
+
             RemoteViews remoteViews = new RemoteViews(this.context.getPackageName(), R.layout.widget_item);
-                              Log.d("listsize",String.valueOf(unsplashList.size()));
-           // if (this.cursor.moveToPosition(position)) {
-                Uri imageUri = Uri.parse(unsplashList.get(position).getUrlRegular());
-                remoteViews.setImageViewUri(R.id.iv_widget,imageUri);
+            appWidgetTarget = new AppWidgetTarget( context, remoteViews, R.id.iv_widget );
 
+            Glide
+                    .with( context.getApplicationContext() ) // safer!
+                    .load(unsplashList.get(position).getUrlRegular())
+                    .asBitmap()
+                    .into( appWidgetTarget );
 
+            Log.d("listsize", String.valueOf(unsplashList.size()));
+            // if (this.cursor.moveToPosition(position)) {
+//            Uri imageUri = Uri.parse(unsplashList.get(position).getUrlRegular());
+//            remoteViews.setImageViewUri(R.id.iv_widget, imageUri);
 
-                Bundle extras = new Bundle();
-                extras.putString("symbol", imageUri.toString());
+//            Glide.with(context)
+//                    .load("http://inthecheesefactory.com/uploads/source/glidepicasso/cover.jpg")
+//                    .into(R.id.iv_widget);
 
+//            Bundle extras = new Bundle();
+//            extras.putString("symbol", imageUri.toString());
+//
+//
+//            Intent fillInIntent = new Intent();
+//            fillInIntent.putExtras(extras);
+//            remoteViews.setOnClickFillInIntent(R.id.ll, fillInIntent);
 
-                Intent fillInIntent = new Intent();
-                fillInIntent.putExtras(extras);
-                remoteViews.setOnClickFillInIntent(R.id.ll, fillInIntent);
+            //   }
 
-         //   }
-
-
+            pushWidgetUpdate(context, remoteViews);
 
             return remoteViews;
         }
+
+        public  void pushWidgetUpdate(Context context, RemoteViews rv) {
+            ComponentName myWidget = new ComponentName(context, WidgetProvider.class);
+            AppWidgetManager manager = AppWidgetManager.getInstance(context);
+            manager.updateAppWidget(myWidget, rv);
+        }
+
+
 
         @Override
         public RemoteViews getLoadingView() {
@@ -118,7 +147,7 @@ unsplashList= new ArrayList<>();
         @Override
         public int getViewTypeCount() {
 
-            return 1;
+            return unsplashList.size();
         }
 
         @Override
