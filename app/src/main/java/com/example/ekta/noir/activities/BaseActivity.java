69acd3f03f78bc619030ed4cd6fdd54515bcaa8f
Ekta Nijhawan <ektanijhawan1@gmail.com>
2017-03-08 11:13:44 +0530

    Keystore - Added

--- app/src/main/java/com/example/ekta/noir/activities/BaseActivity.java
+++ app/src/main/java/com/example/ekta/noir/activities/BaseActivity.java
@@ -19,7 +19,6 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     public Context getContext()
